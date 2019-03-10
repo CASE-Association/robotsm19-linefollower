adf6bc53ec2a58e967f44b91f08459698e560a9a
Isak Åslund <aslundisak@gmail.com>
2019-03-10 18:03:08 +0100

    Encoder implementation (updated)
    Coded in the wrong repository so there was no implementation on the last commit. This one contains the implementation of the encoder.

--- linefollower/encoder.h
+++ linefollower/encoder.h
@@ -7,11 +7,21 @@
 #define HALLA2 7 // left motor sensor
 #define HALLB2 8
 
+// Public functions
 void encoderSetup(void);
 int getLeftEncCount(void);
 int getRightEncCount(void);
 void resetLeftEncCount(void);
 void resetRightEncCount(void);
 
+// Private functions
+void changeA1(void);
+void changeB1(void);
+void changeA2(void);
+void changeB2(void);
+
+
+
 #endif
+
 
